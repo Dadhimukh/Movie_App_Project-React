adc16f418ced26982bf07def6faae870868cd32f
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-25 11:57:55 +0530

    created sstore for movie app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from "redux";
+
+
 import './styles/index.css';
 import App from './components/App';
+import movies from './reducers';
+
+
+const store = createStore(movies);
+console.log('store' , store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
