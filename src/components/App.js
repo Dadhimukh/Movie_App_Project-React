fdffb10088b65dc5a04f73de251d094eff1a74a1
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-03 15:18:45 +0530

    subscribe the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,70 @@
 import { data } from "../data";
+import React from "react";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
+class App extends React.Component {
+  // make call api
+  // dispatch function
+  componentDidMount() {
+    const { store } = this.props;
+    store.subscribe(()=>{
+      console.log("UPDATED");
+      this.forceUpdate();
+    })
 
-        <div className="list">
-          {data.map((movie , index) => {
-            return <MovieCard movie={movie} key={`movie-${index}`} />;
-          })}
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: data,
+    });
+
+          console.log("STATE", this.props.store.getState());
+
+  }
+
+  render() {
+    const movie = this.props.store.getState();
+
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+
+          <div className="list">
+            {movie.map((movie, index) => {
+              return <MovieCard movie={movie} key={`movie-${index}`} />;
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+// function App(props) {
+
+//   const movie = props.store.getState();
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <div className="main">
+//         <div className="tabs">
+//           <div className="tab">Movies</div>
+//           <div className="tab">Favourites</div>
+//         </div>
+
+//         <div className="list">
+//           {movie.map((movie, index) => {
+//             return <MovieCard movie={movie} key={`movie-${index}`} />;
+//           })}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
