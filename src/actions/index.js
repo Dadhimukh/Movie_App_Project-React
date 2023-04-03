4f2d5f3034a0182161aae30848ef74d46a56a839
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-03 15:33:03 +0530

    created Action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
-   // {
-   // type: "ADD_MOVIES";
-   // }
-   // {
-   // type: "DECREASE_COUNT";
-   // }
+// {
+// type: "ADD_MOVIES";
+// }
+// {
+// type: "DECREASE_COUNT";
+// }
+
+// action type
+export const ADD_MOVIES = "ADD_MOVIES";
+
+// action creators
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
+}
