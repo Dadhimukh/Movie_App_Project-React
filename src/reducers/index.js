6d5780a3933b250765328629a14284d26b03f73f
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-06 21:19:25 +0530

    changing the state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { ADD_MOVIES } from "../actions";
-
-export default function movies(state = [], action) {
+const initialMovieState={
+  list:[],
+  favourites:[],
+}
+export default function movies(state = initialMovieState, action) {
   if (action.type === ADD_MOVIES) {
-    return action.movies;
+    return {
+      ...state,
+      list: action.movies,
+    }
   }
   return state;
 }
