86c0d16fd33b8d006541549c14c16d4f9465b3e9
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-07 23:41:03 +0530

    remove if else and use switch case

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,31 @@
-import { ADD_MOVIES } from "../actions";
+import { ADD_MOVIES, ADD_FAV } from "../actions";
 const initialMovieState={
   list:[],
   favourites:[],
 }
 export default function movies(state = initialMovieState, action) {
-  if (action.type === ADD_MOVIES) {
-    return {
-      ...state,
-      list: action.movies,
-    }
+  // if (action.type === ADD_MOVIES) {
+  //   return {
+  //     ...state,
+  //     list: action.movies,
+  //   }
+  // }
+  // return state;
+
+  switch(action.type){
+    case ADD_MOVIES:
+      return{
+        ...state,
+        list:action.movies
+      }
+
+      case ADD_FAV:
+        return{
+          ...state,
+          favourites:[action.movie,...state.favourites]
+        }
+
+        default:
+          return state
   }
-  return state;
 }
