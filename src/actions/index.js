70e8d884e371d1050f2dc83b48414a931c4ae8f5
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-07 23:38:48 +0530

    addFavourite functionality added

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,11 +7,18 @@
 
 // action type
 export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_FAV = "ADD_FAV";
 
 // action creators
 export function addMovies(movies) {
   return {
     type: ADD_MOVIES,
     movies,
+  };
+}
+export function addFavourite(movie) {
+  return {
+    type: ADD_FAV,
+    movie,
   };
 }
