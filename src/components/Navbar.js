5845e072b1800519aae7128bc3740e4aa405191e
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-08 21:05:45 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-   import React, { Component } from "react";
+import React, { Component } from "react";
 
-   export default class Navbar extends Component {
+export default class Navbar extends Component {
    render() {
       return (
          <div className="nav">
@@ -11,4 +11,4 @@
          </div>
       );
    }
-   }
+}
