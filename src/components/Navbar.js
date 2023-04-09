21987f90c10afe129eb8bda4143c148616bd96fc
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-09 23:51:12 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 export default class Navbar extends Component {
-   render() {
-      return (
-         <div className="nav">
-         <div className="search-container">
-            <input />
-            <button id="search-btn">Search</button>
-         </div>
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div className="nav">
+        <div className="search-container">
+          <input />
+          <button id="search-btn">Search</button>
+        </div>
+      </div>
+    );
+  }
 }
